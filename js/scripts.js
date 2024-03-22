728e7db13505f7c306fa9e0d4a5926e7cfa62e88
loganthomasmusic <ltstewart95@gmail.com>
2024-03-21 20:16:09 -0400

    Add spacing between array objects and properties for cleaner code

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
 let pokemonList = [
-    { name: 'Bulbasour', height: 7, type: 'grass'},
-    { name: 'Charmander', height: 6, type: 'fire'},
-    { name: 'Squirtle', height: 5, type: 'water'}
-];
+
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ');
+
+    if (pokemonList[i].height > 6) {
+        document.write("Wow, that's big!")
+    };
+
+    document.write('<br>')
+
+}
+//lists the pokemon by name and height on the DOM, specifying pokemon with over height of 6 as "big" and adding a line break between each object
