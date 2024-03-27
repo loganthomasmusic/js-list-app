0e785872a559bf97ab10b9134f1dbe842e7820ee
loganthomasmusic <ltstewart95@gmail.com>
2024-03-27 11:26:31 -0400

    Add code comments for better clarity

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 let pokemonRepository = (function (){
     
     let pokemonList = [
+    let repository = [ //create repository of pokemon to be called upon using the following functions
 
         {
             name: 'Bulbasour',
@@ -24,15 +25,33 @@ let pokemonRepository = (function (){
 
     function getAll(){
         return pokemonList;
+    function getAll(){ //allow code to call upon the repository
+        return repository;
+    }
+
+    function add(pokemon) { //add new pokemon to the repository
+        repository.push(pokemon);
     }
 
     function add(pokemon) {
         pokemonList.push(pokemon);
+    function addListItem(pokemon) { //creates new pokemon
+        let pokemonList = document.querySelector('.pokemon-list'); //select the pokemon list from html file
+        let listPokemon = document.createElement('li'); //create new list item for the new pokemon
+        let button = document.createElement('button'); //create button for the new pokemon list item
+        button.innerText = pokemon.name; //display pokemon name in the button
+        button.classList.add('button-class'); //add class name to button
+        listPokemon.appendChild(button); //add button to pokemone list item
+        pokemonList.appendChild(listPokemon); //add pokemon to list
     }
 
     return {
         add,
         getAll,
+    return { //return data from IIFE functions
+        add: add,
+        getAll: getAll,
+        addListItem: addListItem
       };
 
 })();
@@ -55,4 +74,5 @@ pokemonRepository.getAll().forEach(function (pokemon) {
         lists the pokemon by name and height on the DOM
         specifies pokemon with height of over 6 as "big"
         adds a line break between each object
-*/
+*/    pokemonRepository.addListItem(pokemon);
+});
